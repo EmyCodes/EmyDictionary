519aacf8aa07e59211ad2343867046c122b49d94
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-19 12:29:47 +0100

    feat: improve to suggest possible word

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ built and developed by OGUNDARE OLAMIDE EMMANUEL
 """
 
 import json
+from difflib import get_close_matches
 
 special_commands = ["Cancel", "Terminate", "Exit"]
+
 with open("data.json", mode="r", encoding="utf-8") as f:
     data = json.load(f)
 
@@ -15,10 +17,22 @@ def translator(word):
     word = word.lower()
     if word in data:
         return data[word]
+
+    # Check for matches
+    suggestions = get_close_matches(word, data.keys())
+
+    if suggestions:
+        suggestion = suggestions[0]
+        user = input(f"Do you mean {suggestion}? (y | n):  ")
+        # user = input(" ")
+        if user.lower() == "y" or user.lower() == "yes":
+            return data[suggestion]
+        else:
+            return "This word doesn't exist. Please, double check it."
     else:
-        return "This word doesn't exist. Please, double check"
+        return "This word doesn't exist. Please, double check it."
 
-
+# Usage
 iterate = True
 while iterate:
     user = input("Enter a word: ")
