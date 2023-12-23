123ff6450748afe0825f0d37a47b3030cef2651a
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-23 18:10:46 +0100

    fix: resolve FileNotFoundError

--- models/data.py
+++ models/data.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python3
+
 import json
+import os
+
 
 def load_data():
-    with open("data.json", mode="r", encoding="utf-8") as f:
+    file_path = os.path.join(os.path.dirname(__file__), "data.json")
+    with open(file_path, mode="r", encoding="utf-8") as f:
         load_data = json.load(f)
         return load_data
