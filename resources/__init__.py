8e37884c5c287208d052aadf903aec6f232645c1
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-24 17:01:31 +0100

    chore: rename

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,2 +1,4 @@
-from resources.interpreter import translator as interpreter
 from resources.base import Base, engine, session
+from resources.error import error_messages
+from resources.interpreter import translator as interpreter
+from resources.interpreter import create_table, insert_data, load_data_from_db
