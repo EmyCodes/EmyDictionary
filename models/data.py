bcba451258ac825ce296e946a24da0b82da4047e
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-24 15:53:34 +0100

    docs: documentation

--- models/data.py
+++ models/data.py
@@ -5,6 +5,12 @@ import os
 
 
 def load_data():
+    """
+    Function to load data from json file and return it as a dictionary object
+    Parameters:
+        load_data(): dictionary object
+        Returns: dictionary object
+    """
     file_path = os.path.join(os.path.dirname(__file__), "data.json")
     with open(file_path, mode="r", encoding="utf-8") as f:
         load_data = json.load(f)
