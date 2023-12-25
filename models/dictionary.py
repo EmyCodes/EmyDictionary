f5bed3af85f0716e1f042866937c54ddee974d07
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-25 11:34:40 +0100

    fix: resolve ImportError

--- models/dictionary.py
+++ models/dictionary.py
@@ -6,6 +6,6 @@ from resources import Base
 class DictionaryModel(Base):
     """To be Updated"""
     __tablename__ = "dictionary"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
     word = Column(String(80), nullable=False)
     meanings = Column(Text)
