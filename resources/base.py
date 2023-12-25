de76b4606d8bd4f9ae46e8719f88d90bd7f695cf
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-25 10:19:49 +0100

    fix: resolve circular import

--- resources/base.py
+++ resources/base.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from models import username, password, host, database
+from config import username, password, host, database
 
 Base = declarative_base()
 
-engine = create_engine(f"mysql+mysqlclient://{username}@{password}:{host}/{database}")
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{database}")
 
 Session = sessionmaker(bind=engine)
 
