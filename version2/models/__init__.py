98509d558ebb3ae09be2b3915981dd2d4c4437aa
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-19 14:04:06 +0100

    chore: restructuring and to be upgraded to version2

--- version2/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# from models.data import data
-from config import username, password, host, database
-# from models.dictionary import DictionaryModel
--- version2/resources/error.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-error_messages = [
-    "This word doesn't exist. Please, double check it.",
-    "Invalid Input. Please, try again with the right word."
-]
