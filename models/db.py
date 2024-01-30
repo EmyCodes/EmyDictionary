5fea2be41d12879acbde59d15e77866d0feb0af6
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-30 15:13:56 +0100

    feat: method insert into table

--- models/db.py
+++ models/db.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
+import json
 import sqlite3
+
+from data import data
 """
 Database Creation
 """
-print(data)
+# for k, v in data.items():
+#     print()
+#     print(f"{k}:  {v}")
 
 class dbModel():
     conn = sqlite3.connect("EmyDictionary.db")
     cur = conn.cursor()
+
     def __init__(self):
         self.cur.execute("CREATE TABLE IF NOT EXISTS EmyDictionary (id INTEGER PRIMARY KEY, keyword STRING, meanings TEXT)")
         self.conn.commit()
-        self.conn.close()
 
-    def insert_into_db(self, keyword, meanings):
-        self.cur.execute("INSERT INTO EmyDictionary VALUES (NULL, ?, ?)", (keyword, meanings))
-        self.conn.commit()
-        self.conn.close()
-        
-    def find(self):
-        pass
-
+    def insert_into_db(self):
+        for key, value in data.items():
+            value_str = json.dumps(value)
+            self.cur.execute("INSERT INTO EmyDictionary VALUES (NULL, ?, ?)", (key, value_str))
+            self.conn.commit()
+            
     def close_db(self):
         self.conn.close()
+        # if self.conn.close:
+        #     print("Closed")
+
+db = dbModel()
+# db.insert_into_db()
+# db.find()
+# db.close_db()
