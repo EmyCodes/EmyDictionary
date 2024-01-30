4306f3b89434e44c654b180efcb95807fedb8cf3
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-30 14:26:09 +0100

    feat: class and method for db

---
+++ models/db.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sqlite3
+"""
+Database Creation
+"""
+
+
+class dbModel():
+    def __init__(self):
+        pass
+
+    def create_table(self):
+        pass
+
+    def find(self):
+        pass
