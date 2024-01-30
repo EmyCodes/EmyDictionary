727c7c864093ac612fe26c094352849af3a275ae
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-30 14:35:13 +0100

    feat: method creates table

--- models/db.py
+++ models/db.py
@@ -6,11 +6,15 @@ Database Creation
 
 
 class dbModel():
+    conn = sqlite3.connect("EmyDictionary.db")
+    cur = conn.cursor()
     def __init__(self):
-        pass
-
-    def create_table(self):
-        pass
+        self.cur.execute("CREATE TABLE IF NOT EXISTS (id INTEGER PRIMARY KEY, keyword STRING, meanings TEXT)")
+        self.conn.commit()
+        self.conn.close()
 
     def find(self):
         pass
+
+    def close_db(self):
+        self.conn.close()
