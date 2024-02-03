a4e2d3ee6492ec50cb9372d4c09ab8660b1486ea
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-02-03 10:12:36 +0100

    feat:entry

--- gui.py
+++ gui.py
@@ -3,6 +3,11 @@ from tkinter import *
 
 root = Tk()
 
+label = Label(root, text="Keyword")
+label.grid(row=0, column=0)
+
+entry = Entry(root, border=2)
+entry.grid(row=0, column=1)
 
 
 root.mainloop()
