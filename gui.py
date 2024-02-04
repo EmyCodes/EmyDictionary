5152396a74bdf305ed43d1fca3553fe4d52d6f95
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-02-04 21:02:18 +0100

    chore: var name change

--- gui.py
+++ gui.py
@@ -3,8 +3,9 @@ from tkinter import *
 
 root = Tk()
 
-label = Label(root, text="Keyword")
-label.grid(row=0, column=0)
+# Labels
+keyword = Label(root, text="Keyword")
+keyword.grid(row=0, column=0)
 
 entry = Entry(root, border=2)
 entry.grid(row=0, column=1)
