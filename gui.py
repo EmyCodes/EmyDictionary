2089230035c7e548841f588db44cffb971a70425
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-02-05 22:17:49 +0100

    feat: close command

--- gui.py
+++ gui.py
@@ -17,7 +17,7 @@ keyword_entry = Entry(root, border=2, textvariable=keyword_text)
 keyword_entry.grid(row=0, column=1)
 
 # Buttons
-close = Button(root, text="Close", width=12, border=3)
+close = Button(root, text="Close", width=12, border=3, command=root.destroy)
 close.grid(row=1, column=5)
 
 search = Button(root, text="Search", width=12, border=3)
@@ -28,6 +28,6 @@ display = Listbox(root, height=6, width=35, border=2)
 display.grid(row=1, column=2, columnspan=3, rowspan=5)
 
 __copyright = Label(root, text="(C) EmyCodes 2024")
-__copyright.grid(row=7, column=2, columnspan=2)
+__copyright.grid(row=10, column=2, columnspan=2)
 
 root.mainloop()
