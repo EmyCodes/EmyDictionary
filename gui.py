97e5519281005364952a37e663ad2d1ac5017bd8
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-02-05 22:30:55 +0100

    feat: gui

--- gui.py
+++ gui.py
@@ -7,25 +7,25 @@ root.wm_title("EmyDictionary")
 
 # Labels
 keyword = Label(root, text="Keyword")
-keyword.grid(row=0, column=0)
+keyword.grid(row=1, column=0)
 
 meaning = Label(root, text="Meaning")
 meaning.grid(row=2, column=0)
 # Entry
 keyword_text = StringVar()
 keyword_entry = Entry(root, border=2, textvariable=keyword_text)
-keyword_entry.grid(row=0, column=1)
+keyword_entry.grid(row=1, column=1)
 
 # Buttons
-close = Button(root, text="Close", width=12, border=3, command=root.destroy)
-close.grid(row=1, column=5)
-
 search = Button(root, text="Search", width=12, border=3)
-search.grid(row=0, column=5)
+search.grid(row=2, column=9)
+close = Button(root, text="Close", width=12, border=3, command=root.destroy)
+close.grid(row=3, column=9)
+
 
 # ListBox
 display = Listbox(root, height=6, width=35, border=2)
-display.grid(row=1, column=2, columnspan=3, rowspan=5)
+display.grid(row=2, column=1, columnspan=3, rowspan=5)
 
 __copyright = Label(root, text="(C) EmyCodes 2024")
 __copyright.grid(row=10, column=2, columnspan=2)
