99fe8bd69a65d2444ab1c1486e2050de95e7fea3
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-02-09 01:22:47 +0100

    chore:clean_up

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from models.db import dbModel
 db = dbModel()
 
 
-def get_matches(n, func):
+def get_matches(keyword, func):
     """
     function to get the closest matches to the word.
     Parameters:\n
@@ -17,17 +17,20 @@ def get_matches(n, func):
     Returns:\n
         str: The closest match to the word.
     """
-    new_sug = []
-    _sug = []
-    _items = func
-    for i in _items:
-        sug = get_close_matches(n, i)
+    _items = func   # stores the list of words from the database
+
+    _sug = []   # a temporary list to store the suggestions (first iteration)
+    new_sug = [] # a new list to store the suggestions from the temporary list above (second iteration)
+    
+    _check = ""  # the closest match to the word
+    for item in _items:
+        sug = get_close_matches(keyword, item)
         if len(sug) != 0:
             _sug.append(sug)
-    for i in _sug:
-        for j in i:
+    for item in _sug:
+        for j in item:
                 new_sug.append(j)
-    _check = get_close_matches(n, new_sug)[0]
+    _check += get_close_matches(keyword, new_sug)[0]
     return _check
 
 
